585348963e7c45a90444f89ba51995d94286b966
Marcel Freg <ndzie.marcellin@kiama.cm>
2026-08-25 21:32:46 +0100

    injection des donnée dans les components et templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { FaceSnap } from './face-snap/face-snap';    
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    FaceSnap
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
