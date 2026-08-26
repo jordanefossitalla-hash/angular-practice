e38fcfe21b0b0498b183d51e552568276d6e633a
Marcel Freg <ndzie.marcellin@kiama.cm>
2026-08-26 20:07:34 +0100

    controle de pipe et ngstyle et class

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap as FaceSnapModel } from '../models/face-snap';
+import {NgStyle, NgClass, UpperCasePipe, DatePipe,DecimalPipe,TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle,
+    NgClass,
+    UpperCasePipe,
+    DatePipe,
+    DecimalPipe,
+    TitleCasePipe
+  ],
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.scss',
 })
